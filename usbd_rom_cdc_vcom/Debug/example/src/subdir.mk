bed1fbc7a3fce588c31f0fad07bbbe2631bec7c5
AlecG74 <31244483+AlecG74@users.noreply.github.com>
2017-08-22 14:36:43 +0200

    LPCXpresso project crc com issue win10
    Project based on the LPCOpen example project : "lpcopen_2_19_lpcxpresso_ngx_xplorer_4330"  "usbd_rom_cdc_vcom"
    The program send 225792 bytes representing an image(392*576) via the virtual serial COM port. It send the data by packet of 256 byte. Each packet is compose of a byte sequence : 0x00,0x01,…,0xFF.

---
+++ usbd_rom_cdc_vcom/Debug/example/src/subdir.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../example/src/cdc_desc.c \
+../example/src/cdc_main.c \
+../example/src/cdc_vcom.c \
+../example/src/cr_startup_lpc43xx.c \
+../example/src/sysinit.c 
+
+OBJS += \
+./example/src/cdc_desc.o \
+./example/src/cdc_main.o \
+./example/src/cdc_vcom.o \
+./example/src/cr_startup_lpc43xx.o \
+./example/src/sysinit.o 
+
+C_DEPS += \
+./example/src/cdc_desc.d \
+./example/src/cdc_main.d \
+./example/src/cdc_vcom.d \
+./example/src/cr_startup_lpc43xx.d \
+./example/src/sysinit.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+example/src/%.o: ../example/src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU C Compiler'
+	arm-none-eabi-gcc -D__REDLIB__ -DDEBUG -D__CODE_RED -D__USE_LPCOPEN -DCORE_M4 -D__MULTICORE_NONE -DNO_BOARD_LIB -I"C:\Users\a.guerin\Documents\projet\FW scanner\test1\lpc_chip_43xx\inc" -I"C:\Users\a.guerin\Documents\projet\FW scanner\test1\usbd_rom_cdc_vcom\example\inc" -I"C:\Users\a.guerin\Documents\projet\FW scanner\test1\lpc_chip_43xx\inc\usbd" -O0 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -fsingle-precision-constant -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=softfp -mthumb -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
